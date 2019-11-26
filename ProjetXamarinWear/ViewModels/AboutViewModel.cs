dfc466c9c83d96b81af6a0181bbe30fcba7fb42c
JohannGolmard <johann.golmard@gmail.com>
2019-11-26 12:30:34 +0100

    Project started

--- ProjetXamarinWear/ViewModels/AboutViewModel.cs
+++ ProjetXamarinWear/ViewModels/AboutViewModel.cs
@@ -2,16 +2,19 @@
 using System.Windows.Input;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 
 namespace ProjetXamarinWear.ViewModels
 {
-    public class AboutViewModel : BaseViewModel
+    public class AboutViewModel : ContentPage
     {
         public AboutViewModel()
         {
             Title = "About";
 
             OpenWebCommand = new Command(() => Device.OpenUri(new Uri("https://xamarin.com/platform")));
+            Map map = new Map();
+            Content = map;
         }
 
         public ICommand OpenWebCommand { get; }
